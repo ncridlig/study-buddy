b45c6087e2b1cd2ad75e03c1a70f1029e7e8792e
ncridlig <ncridlig@ucsd.edu>
2025-07-15 01:19:46 +0200

    domain SSL proxy, good start, not working perfect

--- terraform/k8s/ingress.tf
+++ terraform/k8s/ingress.tf
@@ -1,3 +1,8 @@
+# Provision an IP address
+resource "google_compute_global_address" "ingress_static_ip" {
+  name = "study-buddy-ingress-static-ip"
+}
+
 # Resource: Google-Managed Certificate for the domain
 # This tells GKE to automatically provision and renew an SSL certificate.
 resource "kubernetes_manifest" "managed_cert" {
@@ -20,15 +25,15 @@ resource "kubernetes_manifest" "managed_cert" {
 # Kubernetes Ingress
 # The central piece that routes external traffic to your service
 # and attaches the SSL certificate.
-resource "kubernetes_ingress_v1" "nginx_ingress" {
+resource "kubernetes_ingress_v1" "ingress" {
   metadata {
-    name = "nginx-ingress"
+    name = "ingress"
     namespace = "default"
     annotations = {
       # Use the GCE (Google Cloud) Ingress controller.
       "kubernetes.io/ingress.class" = "gce"
       # Use the static IP you have already reserved.
-      "kubernetes.io/ingress.global-static-ip-name" = "35.195.39.220"
+      "kubernetes.io/ingress.global-static-ip-name" = google_compute_global_address.ingress_static_ip.name
       # Attach the managed certificate by name.
       "networking.gke.io/managed-certificates" = kubernetes_manifest.managed_cert.manifest.metadata.name
       "ingress.kubernetes.io/ssl-redirect"          = "false"
@@ -36,11 +41,45 @@ resource "kubernetes_ingress_v1" "nginx_ingress" {
   }
 
   spec {
-    default_backend {
-      service {
-        name = kubernetes_service_v1.nginx.metadata[0].name
-        port {
-          number = 80
+    rule {
+      http {
+        path {
+          path     = "/api/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service_v1.nginx_internal.metadata[0].name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+
+        path {
+          path      = "/docs"
+          path_type = "Exact"
+          backend {
+            service {
+              name = kubernetes_service_v1.nginx_internal.metadata[0].name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service_v1.frontend_internal.metadata[0].name
+              port {
+                number = 80
+              }
+            }
+          }
         }
       }
     }
