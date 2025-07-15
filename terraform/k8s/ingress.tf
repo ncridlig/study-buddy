2be3f6d6ebded2ba696d084488c30581c0346f62
ncridlig <ncridlig@ucsd.edu>
2025-07-15 20:33:55 +0200

    ingress allows admin Django page

--- terraform/k8s/ingress.tf
+++ terraform/k8s/ingress.tf
@@ -1,9 +1,9 @@
-# Provision an IP address
+# Provision an IP address (move this to infrastructure)
 resource "google_compute_global_address" "ingress_static_ip" {
   name = "study-buddy-ingress-static-ip"
 }
 
-# Resource: Google-Managed Certificate for the domain
+# Resource: Google-Managed Certificate for the domain (move this to infra but takes one hour)
 # This tells GKE to automatically provision and renew an SSL certificate.
 resource "kubernetes_manifest" "managed_cert" {
   manifest = {
@@ -30,11 +30,11 @@ resource "kubernetes_ingress_v1" "ingress" {
     name = "ingress"
     namespace = "default"
     annotations = {
-      # Use the GCE (Google Cloud) Ingress controller.
+      # Use the GCE (Google Cloud) Ingress controller. (get it from remote datasource)
       "kubernetes.io/ingress.class" = "gce"
       # Use the static IP you have already reserved.
       "kubernetes.io/ingress.global-static-ip-name" = google_compute_global_address.ingress_static_ip.name
-      # Attach the managed certificate by name.
+      # Attach the managed certificate by name. (get it from remote datasource)
       "networking.gke.io/managed-certificates" = kubernetes_manifest.managed_cert.manifest.metadata.name
       "ingress.kubernetes.io/ssl-redirect"          = "false"
     }
@@ -55,6 +55,18 @@ resource "kubernetes_ingress_v1" "ingress" {
         path {
           path = "/docs/"
           path_type = "Exact"
+          backend {
+            service {
+              name = kubernetes_service_v1.backend_internal.metadata[0].name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+        path {
+          path = "/admin/"
+          path_type = "Prefix"
           backend {
             service {
               name = kubernetes_service_v1.backend_internal.metadata[0].name
