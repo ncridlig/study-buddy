11119045850fe770da49cf0c437deae86fbcff09
ncridlig <ncridlig@ucsd.edu>
2025-07-16 04:58:10 +0200

    missing status import

--- backend/topics/views.py
+++ backend/topics/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from topics.models import Topic, UploadedFile
 from topics.serializers import TopicSerializer, UploadedFileSerializer
 from topics.docs import schemas
+from rest_framework import status
 
 from drf_yasg.utils import swagger_auto_schema
 
